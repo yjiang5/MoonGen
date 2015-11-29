b4d002e31a388d25acffa6fe5d9964a594e06904
mshahbaz <mshahbaz@cs.princeton.edu>
2015-11-29 03:56:48 -0600

    enabled ieee1588 in dpdk

---
+++ setup-scripts/helpers/setup-vars-moongen-dpdk.sh
@@ -0,0 +1,2 @@
+export RTE_SDK=$PWD/../deps/dpdk
+export RTE_TARGET=x86_64-native-linuxapp-gcc
---
+++ setup-scripts/setup-moongen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ./helpers/setup-vars-moongen-dpdk.sh
+./helpers/setup-hugetlbfs.sh
+./helpers/setup-interfaces.sh
