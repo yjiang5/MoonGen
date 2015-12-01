ffef07115e7107e0cd238bacb02e0ec6d16cf189
mshahbaz <mshahbaz@cs.princeton.edu>
2015-12-01 15:04:14 -0600

    added packet-tx.lua example in custom/

--- examples/custom/packet-tx.lua
+++ examples/custom/packet-tx.lua
@@ -7,26 +7,20 @@ local timer		= require "timer"
 --memory.enableCache()
 
 -- TODO: this
-function master(port1, port2)
-	if not port1 or not port2 then
-		return print("Usage: port1 port2")
+function master(port1, port2, port3)
+	if not port1 or not port2 or not port3 then
+		return print("Usage: port1 port2 port3")
 	end
 	local dev1 = device.config(port1)
 	local dev2 = device.config(port2)
+	local dev3 = device.config(port3)
 	device.waitForLinks()
-	for size = 60, 1518 do
-		print("Running test for packet size = " .. size)
-		local task = dpdk.launchLua("loadSlave", dev1:getTxQueue(0), dev2:getTxQueue(0), size)
-		local avg = task:wait()
-		if not dpdk.running() then
-			break
-		end
-	end
+	local task = dpdk.launchLua("loadSlave", dev1:getTxQueue(0), dev2:getTxQueue(0), dev3:getTxQueue(0))
+	local avg = task:wait()
 	dpdk.waitForSlaves()
 end
 
-
-function loadSlave(queue1, queue2, size)
+function loadSlave(queue1, queue2, queue3)
 	local mem = memory.createMemPool(function(buf)
 		buf:getEthernetPacket():fill{
 			pktLength = size,
@@ -37,17 +31,21 @@ function loadSlave(queue1, queue2, size)
 	bufs = mem:bufArray()
 	local ctr1 = stats:newDevTxCounter(queue1.dev, "plain")
 	local ctr2 = stats:newDevTxCounter(queue2.dev, "plain")
-	local runtime = timer:new(10)
-	while runtime:running() and dpdk.running() do
+	local ctr3 = stats:newDevTxCounter(queue3.dev, "plain")
+	while dpdk.running() do
 		bufs:alloc(size)
 		queue1:send(bufs)
 		ctr1:update()
 		bufs:alloc(size)
 		queue2:send(bufs)
 		ctr2:update()
+		bufs:alloc(size)
+		queue3:send(bufs)
+		ctr3:update()
 	end
 	ctr1:finalize()
 	ctr2:finalize()
+	ctr3:finalize()
 	return nil -- TODO
 end
 
