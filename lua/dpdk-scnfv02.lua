96535778df478eec1d66a37674b3660e36ad51ba
Yunhong Jiang <yunhong.jiang@linux.intel.com>
2016-01-26 16:06:05 -0800

    The dpdk config for the nfv test environment
    Two config file are created for nfv01 and nfv02 test enviroment.
    
    Signed-off-by: Yunhong Jiang <yunhong.jiang@linux.intel.com>

---
+++ lua/dpdk-scnfv02.lua
@@ -0,0 +1,29 @@
+-- configuration for all DPDK command line parameters
+-- see DPDK documentation for more details
+-- MoonGen tries to choose reasonable defaults, so this config file can almost always be empty
+DPDKConfig {
+	-- configure the cores to use, either as a bitmask or as a list
+	-- default: all cores
+	--cores = 0x0F, -- use the first 4 cores
+	cores = {5,7,9,11}, -- 0x66666664
+	
+	-- the number of memory channels (defaults to auto-detect)
+	--memoryChannels = 2,
+
+	-- the configures requried to run multiple DPDK applications. Refer to
+	-- http://dpdk.org/doc/guides/prog_guide/multi_proc_support.html#running-multiple-independent-dpdk-applications
+	-- for more information.
+	
+	-- a string to be the prefix, corresponding to EAL argument "--file-prefix"
+	fileprefix = "nfv02",
+
+	-- A string to specify the socket memory allocation, corresponding to EAL argument "--socket-mem"
+	socketmem = "4096,4096",
+	--
+	-- PCI black list to avoid resetting PCI device assigned to other DPDK apps.
+	-- Corresponding to ELA argument "--pci-blacklist"
+	pciblack = {"0000:01:00.0","0000:01:00.1"},
+
+	-- disable hugetlb, see DPDK documentation for more information
+	--noHugeTlbFs = true,
+}
